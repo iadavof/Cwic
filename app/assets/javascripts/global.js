39b3fa607f705bd4d9dd7a3c02ebb6b381e2fee0
Floris de Lange <florisdelange@iada.nl>
2013-06-10 14:02:02 +0200

    title-aanpassingen template

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -8,30 +8,32 @@ $(document).ready(function() {
   $('#main-menu > li > a').on('click', function() {
     var parent = $(this).parent('li');
     var relatedSubmenu = $('#submenu-box > .submenu[data-main-menu-relation="' + $(parent).attr('id') + '"]');
-    if (!$(parent).hasClass('selected')) {
-      $('#main-menu > li.selected').removeClass('selected');
-      $(parent).addClass('selected');
-      if ($('#submenu-box').height() > 0) {
-        $('#submenu-box').animate({height: 0}, 300, 'swing', function() {
+    if (relatedSubmenu.length > 0) {
+      if (!$(parent).hasClass('selected')) {
+        $('#main-menu > li.selected').removeClass('selected');
+        $(parent).addClass('selected');
+        if ($('#submenu-box').height() > 0) {
+          $('#submenu-box').animate({height: 0}, 300, 'swing', function() {
+            $('#submenu-box > .submenu.selected').removeClass('selected').hide();
+            $('#submenu-box > .submenu[data-main-menu-relation="' + $(parent).attr('id') + '"]').addClass('selected').show();
+            $('#submenu-box').animate({height: relatedSubmenu.outerHeight(false)}, 300);
+          });
+        } else {
           $('#submenu-box > .submenu.selected').removeClass('selected').hide();
           $('#submenu-box > .submenu[data-main-menu-relation="' + $(parent).attr('id') + '"]').addClass('selected').show();
           $('#submenu-box').animate({height: relatedSubmenu.outerHeight(false)}, 300);
-        });
+        }
       } else {
-        $('#submenu-box > .submenu.selected').removeClass('selected').hide();
-        $('#submenu-box > .submenu[data-main-menu-relation="' + $(parent).attr('id') + '"]').addClass('selected').show();
-        $('#submenu-box').animate({height: relatedSubmenu.outerHeight(false)}, 300);
-      }
-    } else {
-      $(parent).removeClass('selected');
-      if ($('#submenu-box').height() > 0) {
-        $('#submenu-box').animate({height: 0}, 300, 'swing', function() {
+        $(parent).removeClass('selected');
+        if ($('#submenu-box').height() > 0) {
+          $('#submenu-box').animate({height: 0}, 300, 'swing', function() {
+            $('#submenu-box > .submenu.selected').removeClass('selected').hide();
+          });
+        } else {
           $('#submenu-box > .submenu.selected').removeClass('selected').hide();
-        });
-      } else {
-        $('#submenu-box > .submenu.selected').removeClass('selected').hide();
+        }
       }
+      return false;
     }
-    return false;
   });
 });
