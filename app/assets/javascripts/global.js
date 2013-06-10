b9ad081ded299f9118dc14d8b64c7e27fa1c0c74
Floris de Lange <florisdelange@iada.nl>
2013-06-10 13:46:41 +0200

    eerste commit van template

---
+++ app/assets/javascripts/global.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $(window).on('resize', function() {
+    var selectedSubmenu = $('#submenu-box > .submenu.selected');
+    $('#submenu-box').css({height: selectedSubmenu.outerHeight(false)});
+  });
+  $('#submenu-box').css({height: 0});
+  $('#submenu-box > .submenu').hide();
+  $('#main-menu > li > a').on('click', function() {
+    var parent = $(this).parent('li');
+    var relatedSubmenu = $('#submenu-box > .submenu[data-main-menu-relation="' + $(parent).attr('id') + '"]');
+    if (!$(parent).hasClass('selected')) {
+      $('#main-menu > li.selected').removeClass('selected');
+      $(parent).addClass('selected');
+      if ($('#submenu-box').height() > 0) {
+        $('#submenu-box').animate({height: 0}, 300, 'swing', function() {
+          $('#submenu-box > .submenu.selected').removeClass('selected').hide();
+          $('#submenu-box > .submenu[data-main-menu-relation="' + $(parent).attr('id') + '"]').addClass('selected').show();
+          $('#submenu-box').animate({height: relatedSubmenu.outerHeight(false)}, 300);
+        });
+      } else {
+        $('#submenu-box > .submenu.selected').removeClass('selected').hide();
+        $('#submenu-box > .submenu[data-main-menu-relation="' + $(parent).attr('id') + '"]').addClass('selected').show();
+        $('#submenu-box').animate({height: relatedSubmenu.outerHeight(false)}, 300);
+      }
+    } else {
+      $(parent).removeClass('selected');
+      if ($('#submenu-box').height() > 0) {
+        $('#submenu-box').animate({height: 0}, 300, 'swing', function() {
+          $('#submenu-box > .submenu.selected').removeClass('selected').hide();
+        });
+      } else {
+        $('#submenu-box > .submenu.selected').removeClass('selected').hide();
+      }
+    }
+    return false;
+  });
+});
