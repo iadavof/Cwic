bd9f7ff01dac9bafd8eaa9f73722ff3d7a377684
Christiaan Thijssen <christiaanthijssen@iada.nl>
2013-06-18 11:57:06 +0200

    organisatie toevoegen bij registratie

--- app/models/organisation_user.rb
+++ app/models/organisation_user.rb
@@ -7,7 +7,14 @@ class OrganisationUser < ActiveRecord::Base
   validates :user, presence: true
   validates :organisation_role, presence: true
 
+  before_validation :set_organisation_role, on: :create
+
   def instance_name
     "#{self.class.model_name.human} #{self.id}"
+  end
+
+  def set_organisation_role
+    # Make the new user Administrator for the new organisation (if we have not yet set the organisation role). This is useful for directly creating an organisation (with nested attributes) while registering the user through Devise.
+    self.organisation_role = OrganisationRole.find_by_name('Administrator') if self.organisation_role.nil? && OrganisationUser.where(user_id: self.user.id, organisation_id: self.organisation.id).empty?
   end
 end
