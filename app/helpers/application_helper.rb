8f56bff23a0ae88525ba6a9c7ccb935102faf4c4
Kevin Reintjes <kevin@krjb.nl>
2013-06-19 13:23:15 +0200

    Entity en Entity Types descriptions en titles wat mooier geformat.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,25 @@ module ApplicationHelper
   def can?(*params)
     true
   end
+
+  # Format description (title) helpers. Useful for (a.o.) entities and entity types.
+  def format_description_title(obj)
+    obj = obj.description if obj.respond_to?(:description)
+    obj = obj.gsub(/\s/i, ' ')
+    obj.truncate(200, separator: ' ')
+  end
+
+  def format_description_with_name_title(obj)
+     if obj.description.present?
+      "#{obj.name} (#{format_description_title(obj)})"
+    else
+      "#{obj.name}"
+    end
+  end
+
+  def format_description(obj)
+    obj = obj.description if obj.respond_to?(:description)
+    obj = obj.gsub(/\s/i, ' ')
+    content_tag(:span, obj.truncate(100, separator: ' '), title: format_description_title(obj))
+  end
 end
