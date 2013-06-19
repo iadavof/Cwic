88542c6aa1810916cb71c63b56708fbfeffc835b
Christiaan Thijssen <christiaanthijssen@iada.nl>
2013-06-19 17:00:49 +0200

    Manager can send invitations

---
+++ spec/views/organisation_users/show.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "organisation_users/show" do
+  before(:each) do
+    @organisation_user = assign(:organisation_user, stub_model(OrganisationUser,
+      :user => nil,
+      :organisation => nil,
+      :organisation_role => nil
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(//)
+    rendered.should match(//)
+    rendered.should match(//)
+  end
+end
