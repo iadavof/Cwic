f58955c20881c1186c5e1368c4fadeeab9ec881b
Christiaan Thijssen <christiaanthijssen@iada.nl>
2013-06-24 11:43:43 +0200

    resend invitation toegevoegd

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Cwic::Application.routes.draw do
   get "home/index"
   root to: 'home#index'
 
-  devise_for :users, controllers: { registrations: "users/registrations", invitations: 'users/invitations' }
+  devise_for :users, controllers: { registrations: "users/registrations", invitations: 'users/invitations' } do
+    match '/users/:user_id/invitation/resend', controller: 'users/invitations', action: 'resend_invitation', via: 'post', as: 'resend_invitation'
+  end
 
   resources :organisations do
     resources :organisation_users, except: [:show, :new]
-    match '/organisation_users/new', :controller => 'organisation_users', :action => 'new', :via => 'post'
+    match '/organisation_users/new', controller: 'organisation_users', action: 'new', via: 'post'
     resources :entity_types
     resources :entities
   end
