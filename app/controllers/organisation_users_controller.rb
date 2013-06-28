b4883361a68ffde20f12f55364fc5477b824ceee
Kevin Reintjes <kreintjes@gmail.com>
2013-06-28 15:59:09 +0200

    Organisation user beheer compleet omgegooid. Alles werkt nu veel cooler, validaties zijn beter, enzovoort. Verder nog een bugje gefixt dat een geinvite user zijn naam kan wijzigen als hij de invite accepteerd.

--- app/controllers/organisation_users_controller.rb
+++ app/controllers/organisation_users_controller.rb
@@ -9,17 +9,7 @@ class OrganisationUsersController < ApplicationController
 
   # GET /organisation_users/new
   def new
-    if params[:email].present?
-      @new_user = User.find_by_email(params[:email])
-      if @new_user.present?
-        @organisation_user.user = @new_user
-        respond_with(@organisation_user)
-      else
-        redirect_to new_user_invitation_path({organisation: @organisation, email: params[:email]})
-      end
-    else
-      redirect_to action: 'index'
-    end
+    respond_with(@organisation_user)
   end
 
   # GET /organisation_users/1/edit
@@ -29,14 +19,37 @@ class OrganisationUsersController < ApplicationController
 
   # POST /organisation_users
   def create
+    # Enable user lookup by email
+    @organisation_user.by_email = true
     @organisation_user.attributes = resource_params
-    @organisation_user.save
-    if @organisation_user.errors[:user].present?
-      flash[:notice] = t('.user_already_has_a_role_in_organisation')
-      redirect_to organisation_organisation_users_path(@organisation_user.organisation)
+    puts @organisation_user.errors.inspect
+    if @organisation_user.valid? && @organisation_user.user.nil?
+      # Organisation user is valid, but user could not be found: render the invitation form for the new user. No need to save the organisation user, this will be done after the invitation is sent (and the user is created).
+      @user = User.new
+      @user.email = @organisation_user.user_email
+      @user.organisation_users << @organisation_user
+      render :new_invitation
     else
+      # User with e-mail already exists: try to save the organisation user and render the normal response.
+      # The before_validation will automatically set the user based on the user_email field.
+      @organisation_user.save
       respond_with(@organisation, @organisation_user, location: organisation_organisation_users_path(@organisation))
     end
+  end
+
+  def send_invitation
+    # The invite call on the user will also calls the save method on this user object and stores the organisation_users relation
+    @user = User.invite!(invite_params, current_user)
+    if @user.valid?
+      respond_with(@organisation, @organisation_user, location: organisation_organisation_users_path(@organisation))
+    else
+      render :new_invitation
+    end
+  end
+
+  def resend_invitation
+    @organisation_user.user.invite!
+    respond_with(@organisation, @organisation_user, location: organisation_organisation_users_path(@organisation))
   end
 
   # PATCH/PUT /organisation_users/1
@@ -56,7 +69,7 @@ private
     case params[:action]
     when 'index'
       @organisation_users = @organisation.organisation_users.accessible_by(current_ability, :index)
-    when 'new', 'create'
+    when 'new', 'create', 'send_invitation'
       @organisation_user = @organisation.organisation_users.build
     else
       @organisation_user = @organisation.organisation_users.find(params[:id])
@@ -64,10 +77,19 @@ private
   end
 
   def resource_params
-    params.require(:organisation_user).permit(:user_id, :organisation_id, :organisation_role_id)
+    params.require(:organisation_user).permit(:user_id, :user_email, :organisation_id, :organisation_role_id)
+  end
+
+  def invite_params
+    params.require(:user).permit(:first_name, :infix, :last_name, :email, :invitation_token, organisation_users_attributes: [:organisation_id, :organisation_role_id])
   end
 
   def interpolation_options
-    { resource_name: @organisation_user.instance_name }
+    user = (params[:action] == 'send_invitation' ? @user : @organisation_user.user)
+    if user.present?
+      { resource_name: user.instance_name, resource_email: user.email }
+    else
+      {}
+    end
   end
 end
