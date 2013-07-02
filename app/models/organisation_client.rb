9cd9fba42dbfa3123a8936c299eaf1bbb8b96529
Christiaan Thijssen <christiaanthijssen@iada.nl>
2013-07-02 10:19:17 +0200

    objectclient fixes

--- app/models/organisation_client.rb
+++ app/models/organisation_client.rb
@@ -3,11 +3,11 @@ class OrganisationClient < ActiveRecord::Base
   has_many :reservations, dependent: :destroy
 
   validates :first_name, presence: true
-  validates :infix, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, length: { maximum: 255 }
 
   def instance_name
-    self.first_name
+    self.first_name + ' ' + (self.infix.present? ? self.infix + ' ' : '') + self.last_name
   end
+
 end
