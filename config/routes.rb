2e1b9fb18f2a57027cb89daa98ef68f217f79734
Kevin Reintjes <kreintjes@gmail.com>
2013-08-21 14:28:07 +0200

    Entity type bewerken nieuwe look & feel gegeven (met name met betrekking tot de property types).

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Cwic::Application.routes.draw do
-
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   get "home/index"
