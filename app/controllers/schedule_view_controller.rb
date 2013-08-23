717498ad3ba891a9337410227b914339c912d4f6
Christiaan Thijssen <christiaanthijssen@iada.nl>
2013-08-23 16:01:43 +0200

    Today and tomorrow view

--- app/controllers/schedule_view_controller.rb
+++ app/controllers/schedule_view_controller.rb
@@ -59,11 +59,79 @@ class ScheduleViewController < ApplicationController
     render json: { entities: result }, status: :ok
   end
 
-  def calculateCurrentProgress(reservation)
-    seconds = reservation.ends_at.time.to_i - reservation.begins_at.time.to_i
+  def today_tomorrow_update
+    result = []
+    entity_types = @organisation.entity_types
+    entity_types.each do |et|
+      entity_type_result = {
+        entity_type_id: et.id,
+        entities: [],
+      }
+
+      et.entities.each do |e|
+        entity_type_result[:entities] << today_tomorrow_update_entity(e)
+      end
+
+      result << entity_type_result
+    end
+    render json: result, status: :ok
   end
 
-  def dayChangeAt(reservation)
+  def today_tomorrow_update_entity(entity)
+    {
+      entity_id: entity.id,
+      current_reservation: today_tomorrow_update_current_reservation(entity),
+      upcoming_reservations: today_tomorrow_update_upcoming_reservation(entity),
+    }
+  end
+
+  def today_tomorrow_update_current_reservation(entity)
+      r = @organisation.reservations.where(entity_id: entity.id).where('begins_at <= :update_moment AND ends_at >= :update_moment', update_moment: Time.now).first
+      if r.present?
+        {
+          item_id: r.id,
+          begin_date: r.begins_at.strftime('%Y-%m-%d'),
+          begin_time: r.begins_at.strftime('%H:%M'),
+          end_date: r.ends_at.strftime('%Y-%m-%d'),
+          end_time: r.ends_at.strftime('%H:%M'),
+          description: r.organisation_client.instance_name,
+          progress: calculate_current_progress(r),
+        }
+      else
+        nil
+      end
+  end
+
+  def today_tomorrow_update_upcoming_reservation(entity)
+      upcoming = {
+        today: get_standard_reservation_info_for_scope(Time.now, Time.now.change({hour: 23, minutes: 59, sec: 59}), entity.id),
+        tomorrow: get_standard_reservation_info_for_scope(Time.now.change({hour: 0, minutes: 0, sec: 0}) + 1.day, Time.now.change({hour: 23, minutes: 59, sec: 59}) + 1.day, entity.id),
+      }
+  end
+
+  def get_standard_reservation_info_for_scope (scope_begin, scope_end, entity_id)
+      res = []
+      reservations = @organisation.reservations.where(entity_id: entity_id).where('begins_at >= :start AND begins_at < :end', start: scope_begin, end: scope_end)
+      reservations.each do |r|
+        res << {
+                          item_id: r.id,
+                          begin_date: r.begins_at.strftime('%Y-%m-%d'),
+                          begin_time: r.begins_at.strftime('%H:%M'),
+                          end_date: r.ends_at.strftime('%Y-%m-%d'),
+                          end_time: r.ends_at.strftime('%H:%M'),
+                          description: r.organisation_client.instance_name,
+                        }
+      end
+      res
+  end
+
+  def calculate_current_progress(reservation)
+    seconds = reservation.ends_at.to_time.to_i - reservation.begins_at.to_time.to_i
+    seconds_past = Time.now.to_i - reservation.begins_at.to_time.to_i
+    (seconds_past.to_f / seconds.to_f * 100.00).round(2)
+  end
+
+  def reservation_day_change_at(reservation)
 
   end
 
