879bfec8d429fb769439fd124f9eb613658ca211
Christiaan Thijssen <christiaanthijssen@iada.nl>
2013-09-08 22:34:29 +0200

    debug messages verwijderd

--- app/assets/javascripts/occupation_view.js
+++ app/assets/javascripts/occupation_view.js
@@ -210,7 +210,6 @@ IADAoccupationView.prototype.createRows = function(response) {
 }
 
 IADAoccupationView.prototype.generateMatrixBlocks = function(maxNr, blockWidth) {
-    console.debug(maxNr, blockWidth);
     var rows = this.occupationContainer.find('div.occupation-matrix-row');
     rows.find('div.occupation-matrix-block').remove();
 
@@ -335,7 +334,6 @@ IADAoccupationView.prototype.getColorForPercentage = function(pct, alpha) {
 IADAoccupationView.prototype.numberOfWeeksInYear = function(year) {
     var fiftyThreeDaysInYears = [4, 9, 15, 20, 26, 32, 37, 43, 48, 54, 60, 65, 71, 76, 82, 88, 93, 99, 105, 111, 116, 122, 128, 133, 139, 144, 150, 156, 161, 167, 172, 178, 184, 189, 195, 201, 207, 212, 218, 224, 229, 235, 240, 246, 252, 257, 263, 268, 274, 280, 285, 291, 296, 303, 308, 314, 320, 325, 331, 336, 342, 348, 353, 359, 364, 370, 376, 381, 387, 392, 398];
     var mod400 = year % 400;
-    console.debug(mod400);
     if(fiftyThreeDaysInYears.indexOf(mod400) > 0) {
         return 53;
     } else return 52;
