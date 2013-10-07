295db72207a6aa5ba9c1067fc5662bf2c5c82788
Kevin Reintjes <kreintjes@gmail.com>
2013-10-07 17:31:28 +0200

    I18n alchemy gem uit officiele github repository pakken ipv onze eigen versie (is niet meer nodig, omdat de normale versie nu ook Rails 4 compatible is).

--- app/models/concerns/datetime_splittable.rb
+++ app/models/concerns/datetime_splittable.rb
@@ -19,7 +19,7 @@ module DatetimeSplittable
         define_method("correct_errors_#{attribute}") { self.correct_errors_date_and_tod(attribute) }
 
         localize "#{attribute}_date", using: :date
-        localize "#{attribute}_tod", using: :time_of_day
+        localize "#{attribute}_tod", using: TimeOfDayParser
 
         if required
           validates "#{attribute}_date", presence: { message: :date_blank }
