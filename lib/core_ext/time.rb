e8b4c8e9dff9c10b3ad5cd6ec4f712ea5e8e0578
Kevin Reintjes <kreintjes@gmail.com>
2013-10-09 11:39:08 +0200

    DatetimeSplittable nog meer verbeterd zodat we nu ook gewoon begins_at en ends_at direct kunnen zetten (nodig voor JavaScript acties). Verder standaardtijden wat logischer gemaakt.

--- lib/core_ext/time.rb
+++ lib/core_ext/time.rb
@@ -1,4 +1,14 @@
+module TimeExtensions
+  %w[ round floor ceil ].each do |_method|
+    define_method "#{_method}_to" do |*args|
+      seconds = args.first || 60
+      Time.at((self.to_f / seconds).send(_method) * seconds)
+    end
+  end
+end
+
 class Time
+  include TimeExtensions
   def to_tod
     TimeOfDay.new(self.hour, self.min, self.sec)
   end
