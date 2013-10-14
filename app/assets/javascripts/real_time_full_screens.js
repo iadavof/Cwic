f717aceb6e3582c6a45cecc05cd1ddd9fb3dd5e9
Floris de Lange <florisdelange@iada.nl>
2013-10-14 11:50:23 +0200

    realtime fullscreens, paginatitel in <title>-tags

--- app/assets/javascripts/real_time_full_screens.js
+++ app/assets/javascripts/real_time_full_screens.js
@@ -1,20 +1,36 @@
 $(document).ready(function() {
   realtimeFullscreensElemPlacement();
+  realtimeFullscreensReservationDatesWidth();
 });
 
 $(window).on('resize', function(){
   realtimeFullscreensElemPlacement();
+  realtimeFullscreensReservationDatesWidth();
 });
 
 function realtimeFullscreensElemPlacement() {
   $('#realtime-list > li > .inner .info-container').each(function(){
     $(this).css('margin-top', ($(this).parent().height() / 2) - ($(this).height() / 2) + 'px');
   });
-  var reservationDatesMaxWidth = 0;
+}
+
+function realtimeFullscreensReservationDatesWidth() {
+  var reservationDatesGreatestWidth = { icon: 0, date: 0, time: 0 };
   $('#realtime-list > li > .inner .reservation-dates').each(function(){
-    if($(this).width() > reservationDatesMaxWidth) {
-      reservationDatesMaxWidth = $(this).width();
+    $(this).find('.datebox .from, .datebox .until').css('width', 'auto');
+    if($(this).find('.datebox .from').first().width() > reservationDatesGreatestWidth['icon']) {
+      reservationDatesGreatestWidth['icon'] = $(this).find('.datebox .from').first().width();
+    }
+    $(this).find('.datebox .date').css('width', 'auto');
+    if($(this).find('.datebox .date').first().width() > reservationDatesGreatestWidth['date']) {
+      reservationDatesGreatestWidth['date'] = $(this).find('.datebox .date').first().width();
+    }
+    $(this).find('.datebox .time').css('width', 'auto');
+    if($(this).find('.datebox .time').first().width() > reservationDatesGreatestWidth['time']) {
+      reservationDatesGreatestWidth['time'] = $(this).find('.datebox .time').first().width();
     }
   });
-  $('#realtime-list > li > .inner .reservation-dates').css('width', reservationDatesMaxWidth + 'px');
+  $('#realtime-list > li > .inner .reservation-dates .datebox .from, #realtime-list > li > .inner .reservation-dates .datebox .until').css('width', reservationDatesGreatestWidth['icon'] + 'px');
+  $('#realtime-list > li > .inner .reservation-dates .datebox .date').css('width', reservationDatesGreatestWidth['date'] + 'px');
+  $('#realtime-list > li > .inner .reservation-dates .datebox .time').css('width', reservationDatesGreatestWidth['time'] + 'px');
 }
