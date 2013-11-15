aa18afdcfd795ccc9b27846cdf5e84ad16d1355d
Floris de Lange <florisdelange@iada.nl>
2013-11-15 23:47:05 +0100

    Fix sticky headers

--- app/assets/javascripts/libraries/jquery.sticky.cwic.js
+++ app/assets/javascripts/libraries/jquery.sticky.cwic.js
@@ -4,42 +4,45 @@
       var container = $('<div class="cwic-sticky-container"></div>');
       sticky.after(container);
       sticky.appendTo(container);
-      var stickyContainer = container.clone();
-      stickyContainer.insertAfter(container).css({position: 'fixed', marginLeft: 0, width: container.outerWidth(false), left: container.offset().left + 'px', top: $('#header').outerHeight(true) + 'px'}).hide().addClass('clone');
-      cwic_sticky.bindEvents(container, stickyContainer);
+      var substituteContainer = $('<div class="cwic-sticky-container clone"></div>');
+      substituteContainer.insertAfter(container).hide();
+      cwic_sticky.bindEvents(container, substituteContainer);
     },
-    bindEvents: function(container, stickyContainer) {
+    bindEvents: function(container, substituteContainer) {
       $('#content-area').on('scroll.cwic_sticky', function(e) {
         var headerHeight = parseFloat($('#header').outerHeight(true));
         var containerOffset = parseFloat(container.offset().top);
-        if (stickyContainer.css('display') == 'none' && (containerOffset < headerHeight)) {
-           cwic_sticky.makeSticky(container, stickyContainer);
+        var substituteContainerOffset = parseFloat(substituteContainer.offset().top);
+        if (substituteContainer.css('display') == 'none' && (containerOffset < headerHeight)) {
+           cwic_sticky.makeSticky(container, substituteContainer);
         }
-        if (stickyContainer.css('display') != 'none' && containerOffset >= headerHeight) {
-          cwic_sticky.destroySticky(container, stickyContainer);
+        if (substituteContainer.css('display') != 'none' && substituteContainerOffset >= headerHeight) {
+          cwic_sticky.destroySticky(container, substituteContainer);
         }
       });
       $(window).on('resize.cwic_sticky header-animated.cwic_sticky', function(e) {
-        cwic_sticky.updateCss(container, stickyContainer);
+        cwic_sticky.updateCss(container, substituteContainer);
       });
       $(window).on('header-start-animation', function(e) {
-        stickyContainer.css({visibility: 'hidden'});
+        container.css({visibility: 'hidden'});
       });
       $(window).on('header-animated', function(e) {
-        stickyContainer.css({visibility: 'visible'});
+        container.css({visibility: 'visible'});
       });
     },
-    makeSticky: function(container, stickyContainer) {
-      container.css({visibility: 'hidden'});
-      cwic_sticky.updateCss(container, stickyContainer);
-      stickyContainer.show();
+    makeSticky: function(container, substituteContainer) {
+      substituteContainer.css({display: 'block'}).attr('style', container.attr('style'));
+      cwic_sticky.updateCss(container, substituteContainer);
     },
-    destroySticky: function(container, stickyContainer) {
-      container.css({visibility: 'visible'});
-      stickyContainer.hide();
+    destroySticky: function(container, substituteContainer) {
+      container.removeAttr('style').attr('style', substituteContainer.attr('style')).css({height: ''});
+      substituteContainer.hide();
     },
-    updateCss: function(container, stickyContainer) {
-      stickyContainer.css({marginLeft: 0, width: container.outerWidth(false) + 'px', left: container.offset().left + 'px', top: $('#header').outerHeight(true) + 'px'});
+    updateCss: function(container, substituteContainer) {
+      if (substituteContainer.css('display') != 'none') {
+        substituteContainer.css({height: container.height() + 'px'});
+        container.css({position: 'fixed', marginLeft: 0, width: substituteContainer.width(), left: substituteContainer.offset().left + 'px', top: $('#header').outerHeight(true) + 'px'});
+      }
     },
   };
   
