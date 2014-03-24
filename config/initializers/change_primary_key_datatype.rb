c4cfaa8f9aee769849109de526a7ae840afd42e3
Kevin Reintjes <kreintjes@gmail.com>
2014-03-24 14:06:26 +0100

    Bugfix in change primary key datatype (aanpassingen werkte niet voor polymorphische associaties)

--- config/initializers/change_primary_key_datatype.rb
+++ config/initializers/change_primary_key_datatype.rb
@@ -6,18 +6,24 @@ ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:prim
 # Force data type of references/associations (reference_id fields) to limit 8 (equals bigint equals bigserial)
 class ActiveRecord::ConnectionAdapters::TableDefinition
   def references_with_limit(*args)
-    options = args.extract_options!
-    options[:limit] = 8 unless options.has_key?(:limit)
-    references_without_limit(*args, options)
+    references_without_limit(*extend_references_options_with_limit(*args))
   end
   alias_method_chain :references, :limit
 end
 
 module ActiveRecord::ConnectionAdapters::SchemaStatements
   def add_reference_with_limit(*args)
-    options = args.extract_options!
-    options[:limit] = 8 unless options.has_key?(:limit)
-    add_reference_without_limit(*args, options)
+    add_reference_without_limit(*extend_references_options_with_limit(*args))
   end
   alias_method_chain :add_reference, :limit
+end
+
+def extend_references_options_with_limit(*args)
+  options = args.extract_options!
+  if options[:polymorphic]
+    # This is a polymorphic association, explicetely set options for type field so we do set a limit for the type field.
+    options[:polymorphic] = options.dup unless options[:polymorphic].is_a?(Hash) # Options were already explicetely set, do not overwrite this.
+  end
+  options[:limit] = 8 unless options.has_key?(:limit) # Set limit to 8 for id field of association
+  [*args, options]
 end
