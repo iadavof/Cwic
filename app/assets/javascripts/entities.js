f803c52bedc44cb6146f2a1a2a3d238878c417a3
Kevin Reintjes <kreintjes@gmail.com>
2014-05-09 11:00:20 +0200

    Kleine codestyle verbetering in entities.js

--- app/assets/javascripts/entities.js
+++ app/assets/javascripts/entities.js
@@ -1,7 +1,6 @@
 APP.entities = {
   init: function() {
-    var form = $('form.new_entity, form.edit_entity');
     $('#entity_color').minicolors();
-    $('div.entity-images-container').magnificPopup({ delegate: 'a', type: 'image',  gallery:{ enabled:true } });
+    $('div.entity-images-container').magnificPopup({ delegate: 'a', type: 'image',  gallery: { enabled: true } });
   }
 }
