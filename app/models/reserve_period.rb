d9e8aec7fc7d2f3c201c1bfeba24c5c68ed5bff2
Kevin Reintjes <kreintjes@gmail.com>
2014-05-26 14:49:11 +0200

    Add ReservePeriod model

---
+++ app/models/reserve_period.rb
@@ -0,0 +1,37 @@
+class ReservePeriod < ActiveRecord::Base
+  include I18n::Alchemy
+
+  belongs_to :entity_type
+  belongs_to :period_unit, class_name: 'TimeUnit'
+
+  validates :entity_type, presence: true
+  validates :name, length: { maximum: 255 }
+  validates :period_amount, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :period_unit_id, presence: true
+  validates :period_unit, presence: true, uniqueness: { scope: [:entity_type, :period_amount] }, if: "period_unit_id.present?" # TODO uniqueness constraint does not work correctly in combination with period_amount == nil (which is actually 1). Fix this when extending this validation to also include check-in and check-out times.
+  validates :price, presence: true, numericality: true
+
+  # Length of the period in seconds
+  def length
+    period_amount * period_unit.seconds
+  end
+
+  # Attribute overwrites
+
+  def name
+    super.presence || default_name
+  end
+
+  def period_amount
+    super || 1
+  end
+
+  def instance_name
+    name
+  end
+
+private
+  def default_name
+    I18n.t('reserve_period.default_name', unit: period_unit.human_numeral_name.lcfirst, unit_plural: period_unit.human_numeral_name(count: 2).lcfirst, count: period_amount) if period_unit.present?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416143325) do
+ActiveRecord::Schema.define(version: 20140516145012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -365,6 +365,21 @@ ActiveRecord::Schema.define(version: 20140416143325) do
   add_index "reservations", ["organisation_client_id"], name: "index_reservations_on_organisation_client_id", using: :btree
   add_index "reservations", ["organisation_id"], name: "index_reservations_on_organisation_id", using: :btree
   add_index "reservations", ["reservation_status_id"], name: "index_reservations_on_reservation_status_id", using: :btree
+
+  create_table "reserve_periods", force: true do |t|
+    t.integer  "entity_type_id", limit: 8
+    t.string   "name"
+    t.integer  "period_amount"
+    t.integer  "period_unit_id", limit: 8
+    t.integer  "min_periods"
+    t.integer  "max_periods"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reserve_periods", ["entity_type_id"], name: "index_reserve_periods_on_entity_type_id", using: :btree
+  add_index "reserve_periods", ["period_unit_id"], name: "index_reserve_periods_on_period_unit_id", using: :btree
 
   create_table "stickies", force: true do |t|
     t.integer  "stickable_id"
