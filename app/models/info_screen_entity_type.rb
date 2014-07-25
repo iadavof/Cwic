be565433751c5b07a663fe13937cbd4494d25545
Kevin Reintjes <kreintjes@gmail.com>
2014-07-25 18:32:26 +0200

    Improve code layout of InfoScreenEntityType model

--- app/models/info_screen_entity_type.rb
+++ app/models/info_screen_entity_type.rb
@@ -1,15 +1,19 @@
 class InfoScreenEntityType < ActiveRecord::Base
+  # Associations
   belongs_to :info_screen
   belongs_to :entity_type
 
   has_many :info_screen_entities, dependent: :destroy, inverse_of: :info_screen_entity_type
   has_many :entities, through: :info_screen_entities
 
+  # Validations
   validates :info_screen, presence: true
   validates :entity_type, presence: true
 
+  # Nested attributes
   accepts_nested_attributes_for :info_screen_entities
 
+  # Scopes
   scope :active, -> { where('active = true') }
 
   def instance_name
