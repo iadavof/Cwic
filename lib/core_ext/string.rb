a3e65e2ac0048d666698684a2533d6f10356c8b8
Kevin Reintjes <kreintjes@gmail.com>
2014-08-11 16:24:53 +0200

    Add String#alphasort comparator

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -24,4 +24,32 @@ class String
   def pl
     return self # Temporary fix to disable the rich_pluralization gem since it is broken with the Rails 4.1.4 upgrade. TODO fix this!
   end
+
+  # Comparator to sort strings such that letters (alphabetic characters) come before numbers and other characters
+  def alphasort(other)
+    length = (self.length < other.length) ? self.length : other.length
+    0.upto(length-1) do |i|
+      # normally we would just return the result of self[i] <=> other[i]. But
+      # you need a custom sorting function.
+      if self[i] == other[i]
+        next # characters the same, skip to next character.
+      else
+        if self[i] =~ /[a-zA-Z]/
+          if other[i] =~ /[a-zA-Z]/
+            return self[i] <=> other[i]  # both alphabetic, sort normally.
+          else
+            return -1  # self is alphabetic, other is not, so self is sorted before.
+          end
+        elsif other[i] =~ /[a-zA-Z]/
+          return 1  # self is not numeric, other is, so self is sorted after.
+        else
+          return self[i] <=> other[i] # both non-alphabetic, sort normally.
+        end
+      end
+    end
+
+    # if we got this far, the segments were identical. However, they may
+    # not be the same length. Short sorted before long.
+    return self.length <=> other.length
+  end
 end
