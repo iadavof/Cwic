32f531e050e66ffe7ff78d9aa3920a9dace41d43
Floris de Lange <florisdelange@iada.nl>
2014-08-13 13:51:57 +0200

    Finish re-organisation of css and js Small styling fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,14 +7,12 @@
 //= require moment
 //= require moment/nl.js
 //= require magnific-popup
-//= require select2
-//= require select2_locale_nl
 //= require websocket_rails/main
+//= require gistyle
 //= require ./shared/i18n/cwic/base
 //= require_tree ./shared/i18n
 //= require_tree ./shared/libraries
 //= require_tree ./application/libraries
-//= require gistyle
-//= require ./shared/util
+//= require_directory ./shared
 //= require ./application/global
 //= require_directory ./application
