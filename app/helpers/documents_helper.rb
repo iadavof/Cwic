ca1471f6fdf67ef9815369149a3e7f2490ed6ab1
Floris de Lange <florisdelange@iada.nl>
2014-08-18 17:36:57 +0200

    Change styling of documents: - add file extension to tiles - restyle information box - put documents in content-block in reservation edit form - do not use block level elements inside <a> elements - add document type icons

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,17 +1,31 @@
 module DocumentsHelper
   def get_document_icon_for_extension(extension)
     extension = extension.downcase
-    if (%w(xlsx pptx docx)).include?(extension)
-      return extension[0..2]
-    elsif (%w(jpg jpeg gif png pdf svg xls ppt doc html eps fla mp3 ai zip rar)).include?(extension)
-      return extension
+    if (%w(tar zip rar gz 7z bz2)).include?(extension)
+      return 'zip'
+    elsif (%w(jpg jpeg gif png bmp svg wmf emf psd ai eps)).include?(extension)
+      return 'photo'
+    elsif (%w(mp3)).include?(extension)
+      return 'sound'
+    elsif (%w(htm html xml)).include?(extension)
+      return 'code'
+    elsif (%w(txt rtf odt ods odp odg odc odf odi odm ott ots otp otg otc otf oti oth indd)).include?(extension)
+      return 'text'
+    elsif (%w(doc docx docm docb dot dotx dotm)).include?(extension)
+      return 'word'
+    elsif (%w(xls xlm xlw xlsx xlsm xlsbxlt xltx xltm)).include?(extension)
+      return 'excel'
+    elsif (%w(ppt pps pptx pptm ppsx ppsm sldx sldm pot potx potm)).include?(extension)
+      return 'powerpoint'
+    elsif (%w(pdf)).include?(extension)
+      return 'pdf'
     else
-      return 'unknown'
+      return 'file'
     end
   end
 
   def get_document_icon_url_for_extension(extension)
     icon_name = get_document_icon_for_extension(extension)
-    ActionController::Base.helpers.asset_path('images/document_icons/' + icon_name + '.png')
+    ActionController::Base.helpers.asset_url('document_icons/' + icon_name + '.png')
   end
 end
