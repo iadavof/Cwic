524a8837d0a1b076338ec18de116a760efd1e41f
Kevin Reintjes <kreintjes@gmail.com>
2014-08-22 11:19:15 +0200

    Refactor and improve ReservationFormController JavaScript class
    (this fixes the warnings in the Reservation form on the slack fields, selected entity and entity list)

--- app/assets/javascripts/application/libraries/reservation_form_controller.cwic.js
+++ app/assets/javascripts/application/libraries/reservation_form_controller.cwic.js
@@ -5,108 +5,74 @@ function CwicReservationFormController(options) {
   }, options || {});
 
   this.formContainer = null;
+  this.slackBeforeField = null;
+  this.slackAfterField = null;
   this.beginMoment = moment();
   this.endMoment = moment();
   this.reservationId = null;
   this.entityTypeId = null;
   this.selectedEntityId = null;
-  this.currentAvailableEntities = [];
+  this.selectedEntityData = null;
+  this.availableEntities = {};
 
   this.init();
 }
 
 CwicReservationFormController.prototype.init = function() {
   this.formContainer = $('#' + this.options.container);
+  this.slackBeforeField = this.formContainer.find('input#reservation_slack_before');
+  this.slackAfterField = this.formContainer.find('input#reservation_slack_after');
   this.reservationId = this.formContainer.data('reservation-id');
   this.bindOnChangeActions();
   this.bindEntitySelection();
-  this.bindSlackFieldValidation();
   this.performFormUpdate();
 };
 
-CwicReservationFormController.prototype.bindSlackFieldValidation = function() {
-  var _this = this;
-  var slackFields = this.formContainer.find('#reservation_slack_before, #reservation_slack_after');
-  slackFields.each(function() {
-    var field = $(this);
-    field.on('change', function() { _this.setSlackFieldErrorState.call(field); });
-  });
-  this.validateSlackFields();
+CwicReservationFormController.prototype.updateSlackFieldsWarningState = function() {
+  this.updateSlackFieldWarningState('before');
+  this.updateSlackFieldWarningState('after');
 };
 
-CwicReservationFormController.prototype.setSlackFieldErrorState = function() {
-  var field = this, current = null;
-  if(field.data('max-slack') != null) {
-    if(field.val() !== '') {
-      current = parseInt(field.val(), 10);
-    } else if(field.attr('placeholder') !== '') { // The value is not set, so we have to check the placeholder value
-      current = parseInt(field.attr('placeholder'), 10);
-    }
-    if(current != null) {
-      APP.util.setFieldErrorState(field, current > parseInt(field.data('max-slack'), 10));
-      return;
-    }
-  }
-
-  APP.util.setFieldErrorState(field, false);
+CwicReservationFormController.prototype.updateSlackFieldWarningState = function(which) {
+  var value = this.slackValue(which);
+  var maxSlack = this.maxSlack(which);
+  APP.util.setFieldErrorState(this.slackField(which), (maxSlack !== null && value > maxSlack));
 };
 
 CwicReservationFormController.prototype.bindEntitySelection = function() {
   var _this = this;
   this.getAvailableEntitiesList().on('click', 'li', function() {
     selectedLi = $(this);
-    _this.selectedEntityId = selectedLi.data('entity-id');
+    _this.setSelectedEntity(selectedLi.data('entity-id'));
     _this.removeSelectedClassFromAvailableEntitiesListItems();
     selectedLi.addClass('selected');
-    _this.setEntitySelection();
   });
 };
 
-CwicReservationFormController.prototype.setEntitySelection = function() {
-  var selCont = this.formContainer.find('div.selected-entity');
-  selCont.text(this.currentAvailableEntities[this.selectedEntityId].name);
-  selCont.removeClass('available unavailable warning-available');
-  selCont.addClass(this.currentAvailableEntities[this.selectedEntityId].warning ? 'warning-available' : 'available');
-
-  // Set the hidden form field for the entity for this reservation
-  this.formContainer.find('input#reservation_entity_id').val(this.selectedEntityId);
-
-  this.setSelectedEntitySlack();
-
-  this.validateSlackFields();
-};
-
-CwicReservationFormController.prototype.validateSlackFields = function() {
-  var _this = this;
-  this.formContainer.find('#reservation_slack_before, #reservation_slack_after').each(function() {
-    _this.setSlackFieldErrorState.call($(this));
-  });
+CwicReservationFormController.prototype.setSelectedEntity = function(entityId) {
+  this.selectedEntityId = entityId;
+  this.selectedEntityData = this.availableEntities[this.selectedEntityId];
+  this.formContainer.find('input#reservation_entity_id').val(this.selectedEntityId); // Set the hidden form field for the entity for this reservation
+  this.formContainer.find('div.selected-entity').text(this.selectedEntityData.name);
+  this.selectedEntityDataChanged();
 };
 
 CwicReservationFormController.prototype.removeSelectedClassFromAvailableEntitiesListItems = function() {
   this.getAvailableEntitiesList().find('li').removeClass('selected');
 };
 
-CwicReservationFormController.prototype.setDefaultSlackTimes = function(default_slack_before, default_slack_after) {
-  this.formContainer.find('input#reservation_slack_before').attr('placeholder', default_slack_before);
-  this.formContainer.find('input#reservation_slack_after').attr('placeholder', default_slack_after);
-};
-
-CwicReservationFormController.prototype.setMaxSlackTimes = function(max_slack_before, max_slack_after) {
-  this.formContainer.find('input#reservation_slack_before').data('max-slack', max_slack_before);
-  this.formContainer.find('input#reservation_slack_after').data('max-slack', max_slack_after);
-};
-
-CwicReservationFormController.prototype.setSelectedEntitySlack = function() {
-  var selEntity = this.currentAvailableEntities[this.selectedEntityId];
-  this.setDefaultSlackTimes(selEntity.default_slack_before, selEntity.default_slack_after);
-  this.setMaxSlackTimes(selEntity.max_slack_before, selEntity.max_slack_after);
+CwicReservationFormController.prototype.updateSlackFieldsPlaceholder = function() {
+  this.slackBeforeField.attr('placeholder', this.defaultSlack('before'));
+  this.slackAfterField.attr('placeholder', this.defaultSlack('after'));
 };
 
 CwicReservationFormController.prototype.bindOnChangeActions = function() {
   var _this = this;
   this.formContainer.find('input#begins_at_date, input#begins_at_tod, input#ends_at_date, input#ends_at_tod, select#entity_type_id').on('change', function() {
-    _this.performFormUpdate.call(_this);
+    _this.performFormUpdate();
+  });
+  this.formContainer.find('input#reservation_slack_before, input#reservation_slack_after').on('change', function() {
+    _this.updateAllSlackWarnings();
   });
 };
 
@@ -118,7 +84,7 @@ CwicReservationFormController.prototype.performFormUpdate = function() {
 };
 
 CwicReservationFormController.prototype.parseSelectedEntityFormField = function() {
-  var selE  = this.formContainer.find('input#reservation_entity_id').val();
+  var selE = this.formContainer.find('input#reservation_entity_id').val();
   if($.isNumeric(selE)) {
     this.selectedEntityId = parseInt(selE, 10);
   } else {
@@ -143,7 +109,7 @@ CwicReservationFormController.prototype.parseBeginAndEndFromFormFields = functio
   var endsAtDateField = this.formContainer.find('input#ends_at_date');
   var endsAtTodField = this.formContainer.find('input#ends_at_tod');
 
-  var beginValid = beginsAtDateField.val() != '' && beginsAtTodField.val() != '';
+  var beginValid = beginsAtDateField.val() !== '' && beginsAtTodField.val() !== '';
 
   var newBeginMoment = moment(beginsAtDateField.datepicker('getDate'));
   this.updateMomentWithTime(newBeginMoment, beginsAtTodField.timepicker('getTime'));
@@ -155,7 +121,7 @@ CwicReservationFormController.prototype.parseBeginAndEndFromFormFields = functio
   APP.util.setFieldErrorState(beginsAtDateField, !beginValid);
   APP.util.setFieldErrorState(beginsAtTodField, !beginValid);
 
-  var endValid = endsAtDateField.val() != '' && endsAtTodField.val() != '';
+  var endValid = endsAtDateField.val() !== '' && endsAtTodField.val() !== '';
 
   var newEndMoment = moment(endsAtDateField.datepicker('getDate'));
   this.updateMomentWithTime(newEndMoment, endsAtTodField.timepicker('getTime'));
@@ -189,25 +155,35 @@ CwicReservationFormController.prototype.updateAvailableEntities = function() {
       begins_at: _this.beginMoment.toJSON(),
       ends_at: _this.endMoment.toJSON()
     }
-  }).success(function(response) {
-    if(response.selected_entity) {
-      _this.updateSelectedEntityFeedback(response.selected_entity);
-    }
-    _this.updateAvailableEntitiesList(response.entities);
-  });
+  }).success(function(response) { _this.updateAvailableEntitiesSuccessCallback(response); });
 };
 
-CwicReservationFormController.prototype.updateAvailableEntitiesList = function(entities) {
+CwicReservationFormController.prototype.updateAvailableEntitiesSuccessCallback = function(response) {
+  _this = this;
+
+  if(response.selected_entity) {
+    this.selectedEntityData = response.selected_entity;
+    this.selectedEntityDataChanged();
+  }
+
+  // Store the available entities data
+  this.availableEntities = {};
+  $.each(response.entities, function(index, entity) {
+    // Cache the entity info for later form settings during entity selection
+    _this.availableEntities[entity.id] = entity;
+  });
+
+  this.renderAvailableEntitiesList();
+};
+
+CwicReservationFormController.prototype.renderAvailableEntitiesList = function() {
   var _this = this;
   this.clearAvailableEntitiesList();
 
   var entityList = this.getAvailableEntitiesList();
-  this.showNoAvailableEntitiesListMessage(entities.length <= 0);
+  this.showNoAvailableEntitiesListMessage(this.availableEntities.length <= 0);
 
-  $.each(entities, function(index, entity) {
-    // Cache the entity info for later form settings during entity selection
-    _this.currentAvailableEntities[entity.id] = entity;
-
+  $.each(this.availableEntities, function(index, entity) {
     var newLi = $('<li>', {
       data: {
         entityId: entity.id
@@ -222,7 +198,7 @@ CwicReservationFormController.prototype.updateAvailableEntitiesList = function(e
       newLi.addClass('selected');
     }
 
-    if(entity.warning) {
+    if(_this.entitySlackOverlap(entity)) {
       newLi.append($('<span>', { 'class': 'icon-warning-sign' }));
     }
 
@@ -234,20 +210,28 @@ CwicReservationFormController.prototype.showNoAvailableEntitiesListMessage = fun
   this.formContainer.find('div.entity-selector div.no-entities-available')[visible ? 'show' : 'hide']();
 };
 
-CwicReservationFormController.prototype.updateSelectedEntityFeedback = function(feedback) {
+CwicReservationFormController.prototype.selectedEntityDataChanged = function() {
+  this.updateSlackFieldsPlaceholder();
+  this.updateSlackFieldsWarningState();
+  this.updateSelectedEntityClass();
+};
+
+CwicReservationFormController.prototype.updateSelectedEntityClass = function() {
   var selCont = this.formContainer.find('div.selected-entity');
   selCont.removeClass('available warning-available unavailable');
-  if(!feedback.available) {
+  if(!this.selectedEntityData.available) {
     selCont.addClass('unavailable');
-  } else if(feedback.warning) {
+  } else if(this.entitySlackOverlap(this.selectedEntityData)) {
     selCont.addClass('warning-available');
   } else {
     selCont.addClass('available');
   }
+};
 
-  this.setDefaultSlackTimes(feedback.default_slack_before, feedback.default_slack_after);
-  this.setMaxSlackTimes(feedback.max_slack_before, feedback.max_slack_after);
-  this.validateSlackFields();
+CwicReservationFormController.prototype.slackFieldsChanged = function() {
+  this.updateSlackFieldsWarningState();
+  this.updateSelectedEntityClass();
+  this.renderAvailableEntitiesList();
 };
 
 CwicReservationFormController.prototype.clearAvailableEntitiesList = function() {
@@ -256,4 +240,34 @@ CwicReservationFormController.prototype.clearAvailableEntitiesList = function() 
 
 CwicReservationFormController.prototype.getAvailableEntitiesList = function() {
   return this.formContainer.find('div.entity-selector ul.entity-list');
+};
+
+CwicReservationFormController.prototype.entitySlackOverlap = function(entity) {
+  return (entity.max_slack_before !== null && this.slackValue('before', entity.default_slack_before) > entity.max_slack_before) || (entity.max_slack_after !== null && this.slackValue('after', entity.default_slack_after) > entity.max_slack_after);
+};
+
+CwicReservationFormController.prototype.slackField = function(which) {
+  return (which == 'before' ? this.slackBeforeField : ( which == 'after' ? this.slackAfterField : null));
+};
+
+CwicReservationFormController.prototype.defaultSlack = function(which) {
+  return this.selectedEntityData['default_slack_'+ which];
+};
+
+CwicReservationFormController.prototype.maxSlack = function(which) {
+  return this.selectedEntityData['max_slack_'+ which];
+};
+
+CwicReservationFormController.prototype.slackValue = function(which, def) {
+  value = this.slackField(which).val();
+  if(typeof def === 'undefined') {
+    def = this.defaultSlack(which);
+  }
+  if(value !== '') {
+    return parseInt(value, 10);
+  } else if(def !== null) { // The value is not set, so we check the default value
+    return def;
+  } else {
+    return null;
+  }
 };
