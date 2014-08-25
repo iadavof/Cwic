e08ac4bb752a37801deb847be19ea3a6d185dae7
Christiaan Thijssen <christiaanthijssen@iada.nl>
2014-08-25 21:02:05 +0200

    Add intro background images (public domain) and random sample code

--- app/helpers/intro_sections_helper.rb
+++ app/helpers/intro_sections_helper.rb
@@ -1,2 +1,9 @@
 module IntroSectionsHelper
+
+	def get_intro_background_images(number)
+		all = Dir.glob("app/assets/images/intro_backgrounds/*")
+		all = all.map { |path| path.gsub("app/assets/images/", "") }
+		all.sample(number)
+	end
+
 end
