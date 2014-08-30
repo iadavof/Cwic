8d78b144ac17a6a00ad03962334bc71c488d4f20
Kevin Reintjes <kreintjes@gmail.com>
2014-08-30 20:18:26 +0200

    Improve code style of OrganisationUsers views

--- app/helpers/organisation_users_helper.rb
+++ app/helpers/organisation_users_helper.rb
@@ -2,9 +2,10 @@ module OrganisationUsersHelper
   def status_actions_for_user(organisation_user)
     case organisation_user.user.status
     when :awaiting_invitation_acceptance
-      return (t('.awaiting_invitation_acceptance') + ' (' + link_to(t('.resend_invitation'), organisation_organisation_user_resend_invitation_path(@organisation, organisation_user), method: 'post') + ')').html_safe
-    else #active
-      return t('.active')
+      resend_link = link_to(t('.resend_invitation'), organisation_organisation_user_resend_invitation_path(@organisation, organisation_user), method: 'post')
+      "#{t('.awaiting_invitation_acceptance')} (#{resend_link})".html_safe
+    else # Active
+      t('.active')
     end
   end
 end
