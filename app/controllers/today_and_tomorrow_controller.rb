6d0d8174cc4ba1a52fbe9e19e91016cee12746d7
Kevin Reintjes <kreintjes@gmail.com>
2014-09-04 10:24:13 +0200

    Refactor TodayAndTomorrowController
    (various small code improvements and cleanups)

--- app/controllers/today_and_tomorrow_controller.rb
+++ app/controllers/today_and_tomorrow_controller.rb
@@ -22,6 +22,20 @@ class TodayAndTomorrowController < ApplicationController
     render json: result, status: :ok
   end
 
+  def current_menu_sub_category
+    :planning
+  end
+
+private
+  def load_resource
+    case params[:action]
+    when 'index'
+      @entity_types = @organisation.entity_types.with_entities.includes(:entities, :icon)
+    when 'update'
+      @entity_types = @organisation.entity_types.with_entities.includes(:entities)
+    end
+  end
+
   def update_entity(entity)
     {
       entity_id: entity.id,
@@ -41,25 +55,32 @@ class TodayAndTomorrowController < ApplicationController
         description: r.instance_name + (r.description.present? ? (' : ' + r.description) : '') + ', ' + r.organisation_client.instance_name,
         progress: r.current_progress,
       }
-      if r.begins_at.to_date != r.ends_at.to_date
-        current[:day_separators] = reservation_day_change_at(r)
-      end
+      current[:day_separators] = reservation_day_change_at(r) unless r.begins_at.to_date == r.ends_at.to_date
       current
     end
   end
 
+  def reservation_day_change_at(reservation)
+    dateChangeAt = []
+    point = reservation.begins_at.end_of_day
+    dateChangeAt << point.to_time
+
+    while(point + 1.day < reservation.ends_at)
+      point = point + 1.day
+      dateChangeAt << point.to_time
+    end
+
+    dateChangeAt.map { |p| reservation.progress_wrt_time_point(p) }
+  end
+
   def update_upcoming_reservation(entity)
     {
-      today: get_standard_reservation_info_for_scope(Time.now, Time.now.end_of_day, entity),
-      tomorrow: get_standard_reservation_info_for_scope(Time.now.beginning_of_day + 1.day, Time.now.end_of_day + 1.day, entity),
+      today: standard_reservation_info_for_scope(Time.now, Time.now.end_of_day, entity),
+      tomorrow: standard_reservation_info_for_scope(Time.now.beginning_of_day + 1.day, Time.now.end_of_day + 1.day, entity),
     }
   end
 
-  def current_menu_sub_category
-    :planning
-  end
-
-  def get_standard_reservation_info_for_scope(scope_begin, scope_end, entity)
+  def standard_reservation_info_for_scope(scope_begin, scope_end, entity)
     res = []
     reservations = entity.reservations.where('begins_at >= :start AND begins_at < :end', start: scope_begin, end: scope_end)
     reservations.each do |r|
@@ -72,26 +93,4 @@ class TodayAndTomorrowController < ApplicationController
     end
     res
   end
-
-  private
-    def load_resource
-      case params[:action]
-      when 'index'
-        @entity_types = @organisation.entity_types.with_entities.includes(:entities, :icon)
-      when 'update'
-        @entity_types = @organisation.entity_types.with_entities.includes(:entities)
-      end
-    end
-
-    def reservation_day_change_at(reservation)
-      dateChangeAt = []
-      point = reservation.begins_at.end_of_day
-      dateChangeAt << point.to_time
-
-      while(point + 1.day < reservation.ends_at)
-        point = point + 1.day
-        dateChangeAt << point.to_time
-      end
-      dateChangeAt.map { |p| reservation.progress_wrt_time_point(p) }
-    end
 end
