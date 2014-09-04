daf75a8ff03528f6142424b9efa6d7e02a7dbb99
Kevin Reintjes <kreintjes@gmail.com>
2014-09-04 11:37:33 +0200

    Rename/move IntroSectionsHelper#get_intro_background_images to IntroductionHelper#intro_background_images

--- app/helpers/introduction_helper.rb
+++ app/helpers/introduction_helper.rb
@@ -1,9 +1,7 @@
-module IntroSectionsHelper
-
-	def get_intro_background_images(number)
+module IntroductionHelper
+	def intro_background_images(number)
 		all = Dir.glob("app/assets/images/intro_backgrounds/*")
 		all = all.map { |path| path.gsub("app/assets/images/", "") }
 		all.sample(number)
 	end
-
 end
