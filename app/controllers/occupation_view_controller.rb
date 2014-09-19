58f2fe240d4f1f231db56e108f6bd6844f67b73f
Kevin Reintjes <kreintjes@gmail.com>
2014-09-19 15:20:05 +0200

    Fix tab indenting in OccupationViewController

--- app/controllers/occupation_view_controller.rb
+++ app/controllers/occupation_view_controller.rb
@@ -1,30 +1,30 @@
 class OccupationViewController < ApplicationController
-	respond_to :json
+  respond_to :json
 
-	def day
-		respond_with(@occupations) do |format|
-			format.html { render }
-			format.json { render_day_json }
-		end
-	end
+  def day
+    respond_with(@occupations) do |format|
+      format.html { render }
+      format.json { render_day_json }
+    end
+  end
 
-	def week
-		respond_with(@occupations) do |format|
-			format.html { render }
-			format.json { render_week_json }
-		end
-	end
+  def week
+    respond_with(@occupations) do |format|
+      format.html { render }
+      format.json { render_week_json }
+    end
+  end
 
 private
-	def render_day_json
-		date = Date.new(params[:year].to_i, params[:month].to_i)
-		day_range = (date.beginning_of_month..date.end_of_month)
-		@occupations = DayOccupation.where(entity: @organisation.entities, day: day_range)
-		render template: 'occupation_view/occupations'
-	end
+  def render_day_json
+    date = Date.new(params[:year].to_i, params[:month].to_i)
+    day_range = (date.beginning_of_month..date.end_of_month)
+    @occupations = DayOccupation.where(entity: @organisation.entities, day: day_range)
+    render template: 'occupation_view/occupations'
+  end
 
-	def render_week_json
-		@occupations = WeekOccupation.where(entity: @organisation.entities, year: params[:year])
-		render template: 'occupation_view/occupations'
-	end
+  def render_week_json
+    @occupations = WeekOccupation.where(entity: @organisation.entities, year: params[:year])
+    render template: 'occupation_view/occupations'
+  end
 end
