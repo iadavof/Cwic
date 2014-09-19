6219da623a0fe0854c3b0d8f076c34ad6704dfcf
Kevin Reintjes <kreintjes@gmail.com>
2014-09-19 15:10:19 +0200

    Refactor UsersController to CrudController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,50 +1,7 @@
-class UsersController < ApplicationController
-  before_action :load_resource
-  authorize_resource
-
-  # GET /users
-  def index
-    respond_with(@users)
-  end
-
-  # GET /users/1
-  def show
-    respond_with(@user)
-  end
-
-  # GET /users/1/edit
-  def edit
-    respond_with(@user)
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    @user.update_attributes(params[:user])
-    respond_with(@user)
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-    respond_with(@user)
-  end
+class UsersController < CrudController
 
 private
-  def load_resource
-    case params[:action]
-    when 'index'
-      @users = User.accessible_by(current_ability, :index).ssp(params)
-    else
-      @user = User.find(params[:id])
-    end
-  end
-
-  # Only allow a trusted parameter "white list" through.
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :infix, :email)
-  end
-
-  def interpolation_options
-    { resource_name: @user.instance_name }
+  def permitted_params
+    [:first_name, :last_name, :infix, :email]
   end
 end
