2a7ab8839578918046c226f60b7137390766dc8f
Kevin Reintjes <kreintjes@gmail.com>
2014-09-26 17:32:10 +0200

    Add disable registrations configuration option

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < DeviseInvitable::RegistrationsController
   before_action :configure_permitted_parameters
+  before_action :disable_registration, if: -> { Rails.application.config.disable_registrations }
 
   def new
     build_resource({})
@@ -10,5 +11,9 @@ class Users::RegistrationsController < DeviseInvitable::RegistrationsController
  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :infix, :email, :password, :password_confirmation, organisations_attributes: [:name,:route, :street_number, :locality, :administrative_area_level_2, :administrative_area_level_1, :country, :postal_code, :address_type, :lng, :lat]) }
+  end
+
+  def disable_registration
+    return redirect_to :root, alert: I18n.t('devise.registrations.disabled')
   end
 end
