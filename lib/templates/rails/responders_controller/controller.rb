e6e516dd335e939baaec2181a766cb0c668aded4
Kevin Reintjes <kreintjes@gmail.com>
2014-09-29 11:23:59 +0200

    Fixup in Controller template

--- lib/templates/rails/responders_controller/controller.rb
+++ lib/templates/rails/responders_controller/controller.rb
@@ -4,9 +4,9 @@ require_dependency "<%= namespaced_file_path %>/application_controller"
 <% end -%>
 <% module_namespacing do -%>
 class <%= controller_class_name %>Controller < CrudController
+<%- unless attributes_names.empty? -%>
   private
 
-<%- unless attributes_names.empty? -%>
   def permitted_params
     [<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>]
   end
