546b6a7d7d97d77ac36108d23476419dce7200b9
Christiaan Thijssen <christiaanthijssen@iada.nl>
2014-10-10 16:20:48 +0200

    Add my organisation menu option

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -2,6 +2,8 @@ class OrganisationsController < CrudController
   respond_to :html, except: :tag_search
   respond_to :json, only: :tag_search
 
+  before_action :update_menu, only: :show
+
   def tag_search
     @tags = @organisation.owned_tags_with_part(params[:tag_part])
     respond_with(@tags)
@@ -11,5 +13,11 @@ class OrganisationsController < CrudController
 
   def permitted_params
    [:name, :phone_general, :phone_reservations, :route, :street_number, :locality, :administrative_area_level_2, :administrative_area_level_1, :country, :postal_code, :address_type, :lng, :lat]
+  end
+
+  def update_menu
+    @current_menu_category = :settings
+    @current_menu_sub_category = :my_organisation
+    @current_menu_link = :details
   end
 end
