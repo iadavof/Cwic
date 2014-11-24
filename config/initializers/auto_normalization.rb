35e24cf9fa763748022a7ec6a7949b84378ee82a
Christiaan Thijssen <christiaanthijssen@iada.nl>
2014-11-24 12:52:09 +0100

    Not so waterproof autonormalization fix

--- config/initializers/auto_normalization.rb
+++ config/initializers/auto_normalization.rb
@@ -2,7 +2,9 @@ Cwic::Application.config.after_initialize do
   Dir.glob("#{Rails.root}/app/models/*.rb").each do |file|
     # Include the AutoNormalizable concern for every ActiveRecord model.
     # Note: we cannot simply add this to ActiveRecord::Base, because the model's database columns need to be known when the concern is included.
-    model = File.basename(file, '.rb').classify.constantize
+    model_name = File.basename(file, '.rb').classify
+    next unless ActiveRecord::Base.connection.table_exists?(model_name.tableize)
+    model = model_name.constantize
     model.send(:include, AutoNormalizable) if model < ActiveRecord::Base && model.table_exists?
   end
 end
