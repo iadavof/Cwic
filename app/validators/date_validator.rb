ee537fd6b0f6440e7dcde104cb40b24cde50aaaa
Kevin Reintjes <kreintjes@gmail.com>
2014-12-09 17:25:21 +0100

    Remove no longer used Date, TimeOfDay and DateAfter validators

--- app/validators/date_validator.rb
+++
@@ -1,5 +0,0 @@
-class DateValidator < ActiveModel::EachValidator
-  def validate_each(record, attr_name, value)
-    record.errors.add(attr_name, :not_a_date, value: value) unless value.is_a?(Date)
-  end
-end
