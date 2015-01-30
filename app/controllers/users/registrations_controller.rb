0a3cafb3583c949dc4b67f4327c9c00d136fde21
Kevin Reintjes <kreintjes@gmail.com>
2015-01-30 12:24:07 +0100

    Small code style improvement of RegistrationsController

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,7 +11,10 @@ class Users::RegistrationsController < DeviseInvitable::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :infix, :email, :password, :password_confirmation, organisations_attributes: [:name,:route, :street_number, :locality, :administrative_area_level_2, :administrative_area_level_1, :country, :postal_code, :address_type, :lng, :lat]) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
+      :first_name, :last_name, :infix, :email, :password, :password_confirmation,
+      organisations_attributes: [:name,:route, :street_number, :locality, :administrative_area_level_2, :administrative_area_level_1, :country, :postal_code, :address_type, :lng, :lat])
+    }
   end
 
   def disable
